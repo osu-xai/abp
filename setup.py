747489413f73a1b386e37ead7d21e8db56a2444c
Lawrence Neal <nealla@compute-gpu2.hpc.engr.oregonstate.edu>
2018-10-16 02:49:43 +0000

    Remove dependencies that don't match requirements.txt

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIRED_PACKAGES = ["torch==0.4", "gym", "numpy", "visdom", "tensorboardX"]
-
-
 setup(name='abp',
-      version='0.1',
+      version='0.1.1',
       description='Reinforcement Learning Library in python',
       author='Magesh Kumar Murali',
       author_email='m.magesh.66@gmail.com',
       include_package_data=True,
       package_data={'': ['tasks']},
       packages=find_packages(),
-      install_requires=REQUIRED_PACKAGES
-      )
+)
