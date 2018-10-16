37320ab94c9393edc7d433e73c6fe9a6f17d460f
Lawrence Neal <nealla@compute-gpu2.hpc.engr.oregonstate.edu>
2018-10-15 19:51:01 -0700

    Update build/lib/abp to newest version

--- build/lib/abp/adaptives/hra/adaptive.py
+++ build/lib/abp/adaptives/hra/adaptive.py
@@ -1,17 +1,27 @@
 import logging
+import time
+import random
+import pickle
+import os
+import sys
 
-logger = logging.getLogger('root')
-
-from copy import deepcopy
-import numpy as np
 import torch
-from torch.autograd import Variable
+from baselines.common.schedules import LinearSchedule
+
+
 from abp.adaptives.common.prioritized_memory.memory import PrioritizedReplayBuffer
 from abp.utils import clear_summary_path
 from abp.models import HRAModel
 from tensorboardX import SummaryWriter
 
-from baselines.common.schedules import LinearSchedule
+
+logger = logging.getLogger('root')
+use_cuda = torch.cuda.is_available()
+FloatTensor = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
+LongTensor = torch.cuda.LongTensor if use_cuda else torch.LongTensor
+IntTensor = torch.cuda.IntTensor if use_cuda else torch.IntTensor
+ByteTensor = torch.cuda.ByteTensor if use_cuda else torch.ByteTensor
+Tensor = FloatTensor
 
 
 class HRAAdaptive(object):
@@ -23,106 +33,147 @@ class HRAAdaptive(object):
         self.choices = choices
         self.network_config = network_config
         self.reinforce_config = reinforce_config
-        self.update_frequency = reinforce_config.update_frequency
-
-        self.replay_memory = PrioritizedReplayBuffer(self.reinforce_config.memory_size, 0.6)
+        self.replace_frequency = reinforce_config.replace_frequency
+        self.memory = PrioritizedReplayBuffer(self.reinforce_config.memory_size, 0.6)
         self.learning = True
-        self.explanation = False
-
-        self.steps = 0
-        self.previous_state = None
-        self.previous_action = None
         self.reward_types = reward_types
-
-        self.clear_rewards()
-
-
-        self.total_reward = 0
-
-        self.eval_model = HRAModel(self.name + "_eval", self.network_config)
-        self.target_model = HRAModel(self.name + "_target", self.network_config)
-
-        clear_summary_path(self.reinforce_config.summaries_path + "/" + self.name)
-        self.summary = SummaryWriter(log_dir = self.reinforce_config.summaries_path + "/" + self.name)
-
-
+        self.steps = 0
         self.episode = 0
-        self.beta_schedule = LinearSchedule(10 * 1000, initial_p = 0.2, final_p = 1.0)
+        self.reward_history = []
+        self.best_reward_mean = -sys.maxsize
 
+        self.beta_schedule = LinearSchedule(self.reinforce_config.beta_timesteps,
+                                            initial_p=self.reinforce_config.beta_initial,
+                                            final_p=self.reinforce_config.beta_final)
+
+        self.epsilon_schedule = LinearSchedule(self.reinforce_config.epsilon_timesteps,
+                                               initial_p=self.reinforce_config.starting_epsilon,
+                                               final_p=self.reinforce_config.final_epsilon)
+
+        self.reset()
+
+        self.eval_model = HRAModel(self.name + "_eval", self.network_config, use_cuda)
+        self.target_model = HRAModel(self.name + "_target", self.network_config, use_cuda)
+
+        reinforce_summary_path = self.reinforce_config.summaries_path + "/" + self.name
+
+        if not network_config.restore_network:
+            clear_summary_path(reinforce_summary_path)
+        else:
+            self.restore_state()
+
+        self.summary = SummaryWriter(log_dir=reinforce_summary_path)
 
     def __del__(self):
-        self.summary.close()
-
+        self.save()
+        if hasattr(self, 'summary'):
+            self.summary.close()
 
     def should_explore(self):
-        epsilon = np.max([0.1, self.reinforce_config.starting_epsilon * (
-                         self.reinforce_config.decay_rate ** (self.steps / self.reinforce_config.decay_steps))])
+        self.epsilon = self.epsilon_schedule.value(self.steps)
+        self.summary.add_scalar(tag='%s/Epsilon' % self.name,
+                                scalar_value=self.epsilon,
+                                global_step=self.steps)
 
-        self.summary.add_scalar(tag='%s/Epsilon' % self.name, scalar_value=epsilon, global_step=self.steps)
-
-        return np.random.choice([True, False], p=[epsilon, 1 - epsilon])
-
+        return random.random() < self.epsilon
 
     def predict(self, state):
         self.steps += 1
 
-        if self.previous_state is not None and self.previous_action is not None:
-            self.replay_memory.add(self.previous_state, self.previous_action, self.reward_list(), state, False)
+        if (self.previous_state is not None and
+                self.previous_action is not None):
+            self.memory.add(self.previous_state,
+                            self.previous_action,
+                            self.reward_list(),
+                            state, 0)
 
         if self.learning and self.should_explore():
-            action = np.random.choice(len(self.choices))
-            q_values = [None] * len(self.choices)  # TODO should it be output shape or from choices?
+            action = random.choice(list(range(len(self.choices))))
+            q_values = None
+            combined_q_values = None
             choice = self.choices[action]
         else:
-            _state = Variable(torch.Tensor(state)).unsqueeze(0)
-            action, q_values = self.eval_model.predict(_state)
-
+            _state = Tensor(state).unsqueeze(0)
+            model_start_time = time.time()
+            action, q_values, combined_q_values = self.eval_model.predict(_state,
+                                                                          self.steps,
+                                                                          self.learning)
             choice = self.choices[action]
+            self.model_time += time.time() - model_start_time
 
-        if self.learning and self.steps % self.update_frequency == 0:
+        if self.learning and self.steps % self.replace_frequency == 0:
             logger.debug("Replacing target model for %s" % self.name)
             self.target_model.replace(self.eval_model)
 
+        if (self.learning and
+                self.steps > self.reinforce_config.update_start and
+                self.steps % self.reinforce_config.update_steps == 0):
 
-        self.update()
+            update_start_time = time.time()
+            self.update()
+            self.update_time += time.time() - update_start_time
 
-        self.clear_rewards()
+        self.clear_current_rewards()
 
         self.previous_state = state
         self.previous_action = action
 
-        return choice, q_values
+        return choice, q_values, combined_q_values
 
     def disable_learning(self):
         logger.info("Disabled Learning for %s agent" % self.name)
-        self.eval_model.save_network()
-        self.target_model.save_network()
+        self.save()
 
         self.learning = False
         self.episode = 0
-
 
     def end_episode(self, state):
         if not self.learning:
             return
 
-        logger.info("End of Episode %d with total reward %.2f" % (self.episode + 1, self.total_reward))
+        self.reward_history.append(self.total_reward)
+
+        logger.info("End of Episode %d with total reward %.2f, epsilon %.2f" %
+                    (self.episode + 1, self.total_reward, self.epsilon))
 
         self.episode += 1
+        self.summary.add_scalar(tag='%s/Episode Reward' % self.name,
+                                scalar_value=self.total_reward,
+                                global_step=self.episode)
 
-        self.summary.add_scalar(tag  = '%s/Episode Reward' % self.name,
-                                    scalar_value = self.total_reward,
-                                    global_step = self.episode)
+        for reward_type in self.reward_types:
+            tag = '%s/Decomposed Reward/%s' % (self.name, reward_type)
+            value = self.decomposed_total_reward[reward_type]
+            self.summary.add_scalar(tag=tag, scalar_value=value, global_step=self.episode)
 
-        self.replay_memory.add(self.previous_state, self.previous_action, self.reward_list(), state, True)
+        self.memory.add(self.previous_state,
+                        self.previous_action,
+                        self.reward_list(),
+                        state, 1)
 
-        self.clear_rewards()
-        self.total_reward = 0
+        self.episode_time = time.time() - self.episode_time
+
+        logger.debug("Episode Time: %.2f, "
+                     "Model prediction time: %.2f, "
+                     "Updated time: %.2f, "
+                     "Update fit time: %.2f" % (self.episode_time,
+                                                self.model_time,
+                                                self.update_time,
+                                                self.fit_time))
+
+        self.save()
+        self.reset()
+
+    def reset(self):
+        self.clear_current_rewards()
+        self.clear_episode_rewards()
 
         self.previous_state = None
         self.previous_action = None
-
-        self.update()
+        self.episode_time = time.time()
+        self.update_time = 0
+        self.fit_time = 0
+        self.model_time = 0
 
     def reward_list(self):
         reward = [0] * len(self.reward_types)
@@ -132,53 +183,107 @@ class HRAAdaptive(object):
 
         return reward
 
-    def clear_rewards(self):
+    def clear_current_rewards(self):
         self.current_reward = {}
         for reward_type in self.reward_types:
             self.current_reward[reward_type] = 0
 
+    def clear_episode_rewards(self):
+        self.total_reward = 0
+        self.decomposed_total_reward = {}
+        for reward_type in self.reward_types:
+            self.decomposed_total_reward[reward_type] = 0
 
     def reward(self, reward_type, value):
         self.current_reward[reward_type] += value
+        self.decomposed_total_reward[reward_type] += value
         self.total_reward += value
 
+    def restore_state(self):
+        restore_path = self.network_config.network_path + "/adaptive.info"
+
+        if self.network_config.network_path and os.path.exists(restore_path):
+            logger.info("Restoring state from %s" % self.network_config.network_path)
+
+            with open(restore_path, "rb") as file:
+                info = pickle.load(file)
+
+            self.steps = info["steps"]
+            self.best_reward_mean = info["best_reward_mean"]
+            self.episode = info["episode"]
+
+            logger.info("Continuing from %d episode (%d steps) with best reward mean %.2f" %
+                        (self.episode, self.steps, self.best_reward_mean))
+
+    def save(self, force=False):
+        info = {
+            "steps": self.steps,
+            "best_reward_mean": self.best_reward_mean,
+            "episode": self.episode
+        }
+
+        if force:
+            logger.info("Forced to save network")
+            self.eval_model.save_network()
+            self.target_model.save_network()
+            pickle.dump(info, self.network_config.network_path + "adaptive.info")
+
+        if (len(self.reward_history) >= self.network_config.save_steps and
+                self.episode % self.network_config.save_steps == 0):
+
+            total_reward = sum(self.reward_history[-self.network_config.save_steps:])
+            current_reward_mean = total_reward / self.network_config.save_steps
+
+            if True: #current_reward_mean >= self.best_reward_mean:
+                self.best_reward_mean = current_reward_mean
+                info["best_reward_mean"] = current_reward_mean
+                logger.info("Saving network. Found new best reward (%.2f)" % current_reward_mean)
+                self.eval_model.save_network()
+                self.target_model.save_network()
+                with open(self.network_config.network_path + "/adaptive.info", "wb") as file:
+                    pickle.dump(info, file, protocol=pickle.HIGHEST_PROTOCOL)
+            else:
+                logger.info("The best reward is still %.2f. Not saving" % current_reward_mean)
 
     def update(self):
-        if self.steps <= self.reinforce_config.batch_size:
+        if len(self.memory) <= self.reinforce_config.batch_size:
             return
 
         beta = self.beta_schedule.value(self.steps)
+        self.summary.add_scalar(tag='%s/Beta' % self.name,
+                                scalar_value=beta, global_step=self.steps)
 
-        self.summary.add_scalar(tag='%s/Beta' % self.name, scalar_value=beta, global_step=self.steps)
+        batch = self.memory.sample(self.reinforce_config.batch_size, beta)
 
-        states, actions, reward, next_states, is_terminal, weights, batch_idxes = self.replay_memory.sample(self.reinforce_config.batch_size, beta)
-        
-        states = Variable(torch.Tensor(states))
-        next_states = Variable(torch.Tensor(next_states))
+        (states, actions, reward, next_states,
+         is_terminal, weights, batch_idxes) = batch
 
-        is_terminal = [0 if t else 1 for t in is_terminal]
+        self.summary.add_histogram(tag='%s/Batch Indices' % self.name,
+                                   values=Tensor(batch_idxes),
+                                   global_step=self.steps)
 
-        q_next = self.target_model.predict_batch(next_states)
+        states = Tensor(states)
+        next_states = Tensor(next_states)
+        terminal = FloatTensor(is_terminal)
+        reward = FloatTensor(reward)
+        batch_index = torch.arange(self.reinforce_config.batch_size,
+                                   dtype=torch.long)
 
-        q_2 = np.mean(q_next, axis = 2)
+        # Find the target values
+        q_actions, q_values, _ = self.eval_model.predict_batch(states)
+        q_values = q_values[:, batch_index, actions]
+        _, q_next, _ = self.target_model.predict_batch(next_states)
+        q_next = q_next.mean(2).detach()
+        q_next = (1 - terminal) * q_next
+        q_target = reward.t() + self.reinforce_config.discount_factor * q_next
 
-        q_2 = is_terminal * q_2
+        # Update the model
+        fit_start_time = time.time()
+        self.eval_model.fit(q_values, q_target, self.steps)
+        self.fit_time += time.time() - fit_start_time
 
-        q_values = self.eval_model.predict_batch(states)
-
-        q_target = q_values.copy()
-
-        batch_index = np.arange(self.reinforce_config.batch_size, dtype=np.int32)
-
-        q_target[:, batch_index, actions] = np.transpose(reward) + self.reinforce_config.discount_factor * q_2
-
-        td_errors = q_values[:, batch_index, actions] - q_target[:, batch_index, actions]
-
-        td_errors = td_errors.sum(axis=0)
-
-        new_priorities = np.abs(td_errors) + 1e-6 #prioritized_replay_eps
-        self.replay_memory.update_priorities(batch_idxes, new_priorities)
-
-        self.eval_model.fit(states, q_target, self.steps)
-
-        return td_errors
+        # Update priorities
+        td_errors = q_values - q_target
+        td_errors = torch.sum(td_errors, 0)
+        new_priorities = torch.abs(td_errors) + 1e-6  # prioritized_replay_eps
+        self.memory.update_priorities(batch_idxes, new_priorities.data)
--- build/lib/abp/models/dqn_model.py
+++ build/lib/abp/models/dqn_model.py
@@ -1,51 +1,133 @@
 import logging
-import torch.nn as nn
-from torch.optim import RMSprop
-from .model import Model
+
 import numpy as np
-import excitationbp as eb
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+
+from .model import Model
+from abp.utils import clear_summary_path
+from abp.utils.models import generate_layers
+from tensorboardX import SummaryWriter
 
 logger = logging.getLogger('root')
 
+
+def weights_initialize(module):
+    if type(module) == nn.Linear:
+        nn.init.xavier_uniform_(module.weight, gain=nn.init.calculate_gain('relu'))
+        module.bias.data.fill_(0.01)
+
+
 class _DQNModel(nn.Module):
-    """Neural Network for the DQN algorithm """
+    """ Model for DQN """
 
     def __init__(self, network_config):
         super(_DQNModel, self).__init__()
-        self.num_layers = len(network_config.layers)
-        in_features = int(np.prod(network_config.input_shape))
-        for i, out_features in enumerate(network_config.layers):
-            layer = nn.Sequential(
-                nn.Linear(in_features, out_features),
-                nn.ReLU())
-            in_features = out_features
-            setattr(self, 'layer_{}'.format(i), layer)
-        self.q_linear = nn.Linear(in_features, network_config.output_shape[0])
+
+        layer_modules, input_shape = generate_layers(network_config.input_shape,
+                                                     network_config.layers)
+
+        layer_modules["OutputLayer"] = nn.Linear(int(np.prod(input_shape)),
+                                                 network_config.output_shape)
+
+        self.layers = nn.Sequential(layer_modules)
+        self.layers.apply(weights_initialize)
 
     def forward(self, input):
-        out = input.view((input.shape[0], int(np.prod(input.shape[1:]))))
-        for i in range(self.num_layers):
-            out = getattr(self, 'layer_{}'.format(i))(out)
-        return self.q_linear(out)
+        x = input
+        for layer in self.layers:
+            if type(layer) == nn.Linear:
+                x = x.view(-1, int(np.prod(x.shape[1:])))
+            x = layer(x)
+        return x
 
 
 class DQNModel(Model):
 
-    def __init__(self, name, network_config, restore=True, learning_rate=0.001):
-        logger.info("Building network for %s" % name)
-        self.network_config = network_config
+    def __init__(self, name, network_config, use_cuda, restore=True, learning_rate=0.001):
+        self.name = name
         model = _DQNModel(network_config)
-        Model.__init__(self, model, name, network_config, restore)
-        logger.info("Created network for %s " % self.name)
-        self.optimizer = RMSprop(self.model.parameters(), lr=learning_rate)
-        self.loss_fn = nn.MSELoss()
+        model = nn.DataParallel(model)
+        self.use_cuda = use_cuda
 
-    def fit(self, states, target, steps):
+        if use_cuda:
+            logger.info("Network %s is using cuda " % self.name)
+            model = model.cuda()
+
+        super(DQNModel, self).__init__(model, name, network_config, restore)
+        self.network_config = network_config
+        self.optimizer = Adam(self.model.parameters(), lr=self.network_config.learning_rate)
+        self.loss_fn = nn.SmoothL1Loss()
+
+        summaries_path = self.network_config.summaries_path + "/" + self.name
+
+        if not network_config.restore_network:
+            clear_summary_path(summaries_path)
+            self.summary = SummaryWriter(log_dir=summaries_path)
+        else:
+            self.summary = SummaryWriter(log_dir=summaries_path)
+
+        logger.info("Created network for %s " % self.name)
+
+    def weights_summary(self, steps):
+        model = self.model.module
+
+        for i, layer in enumerate(self.network_config.layers):
+            if layer["type"] in ["BatchNorm2d", "MaxPool2d"]:
+                continue
+
+            layer_name = layer["name"] if "name" in layer else "Layer_%d" % i
+            weight_name = '{}/{}/weights'.format(self.name, layer_name)
+            bias_name = '{}/{}/bias'.format(self.name, layer_name)
+
+            module = getattr(model.layers, layer_name)
+
+            if type(module) == nn.DataParallel:
+                module = module.module
+
+            weight = module.weight.clone().data
+            bias = module.bias.clone().data
+            self.summary.add_histogram(tag=weight_name, values=weight, global_step=steps)
+            self.summary.add_histogram(tag=bias_name, values=bias, global_step=steps)
+
+        weight_name = '{}/Output Layer/weights'.format(self.name, i)
+        bias_name = '{}/Output Layer/bias'.format(self.name, i)
+
+        module = getattr(model.layers, "OutputLayer")
+        weight = module.weight.clone().data
+        bias = module.bias.clone().data
+
+        self.summary.add_histogram(tag=weight_name, values=weight, global_step=steps)
+        self.summary.add_histogram(tag=bias_name, values=bias, global_step=steps)
+
+    def predict(self, input, steps, learning):
+        q_values = self.model(input).squeeze(1)
+        action = torch.argmax(q_values)
+
+        if steps % self.network_config.summaries_step == 0 and learning:
+            logger.debug("Adding network summaries!")
+            self.weights_summary(steps)
+            self.summary.add_histogram(tag="%s/Q values" % (self.name),
+                                       values=q_values.clone().cpu().data.numpy(),
+                                       global_step=steps)
+
+        return action.item(), q_values
+
+    def predict_batch(self, input):
+        if self.use_cuda:
+            input = input.cuda()
+        q_values = self.model(input)
+        values, q_actions = q_values.max(1)
+        return q_actions, q_values
+
+    def fit(self, q_values, target_q_values, steps):
+        loss = self.loss_fn(q_values, target_q_values)
+
         self.optimizer.zero_grad()
-        predict = self.model(states)
-        loss = self.loss_fn(predict, target)
         loss.backward()
         self.optimizer.step()
 
-    def predict(self, input):
-        return self.model(input)
+        self.summary.add_scalar(tag="%s/Loss" % (self.name),
+                                scalar_value=float(loss),
+                                global_step=steps)
