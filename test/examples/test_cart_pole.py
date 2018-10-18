0fb80b5f8cd9fd4d7eafce76263c5548183407d8
Lawrence Neal <nealla@lwneal.com>
2018-10-18 13:39:08 -0700

    Fix cartpole config directory

--- test/examples/test_cart_pole.py
+++ test/examples/test_cart_pole.py
@@ -5,6 +5,6 @@ class CartPoleTests(unittest.TestCase):
     def test_cart_pole_dqn(self):
         sys.argv = ['',
                 '--task', 'abp.examples.open_ai.cart_pole.dqn',
-                '--folder', 'test/tasks/cart_pole_dqn_v1']
+                '--folder', 'test/tasks/cart_pole_dqn']
         from abp.trainer.task_runner import main
         main()
