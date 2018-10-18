58f38470b9fe75dcf6f0e40c668601406e82240e
Lawrence Neal <nealla@lwneal.com>
2018-10-18 13:47:07 -0700

    Add helpful log output

--- abp/examples/open_ai/fruit_collection/dqn.py
+++ abp/examples/open_ai/fruit_collection/dqn.py
@@ -11,11 +11,16 @@ from abp.openai import envs
 
 
 def run_task(evaluation_config, network_config, reinforce_config):
+    start_time = time.time()
+    print('Initializing FruitCollection environment...')
     env = gym.make(evaluation_config.env)
     state = env.reset(state_representation="linear")
     LEFT, RIGHT, UP, DOWN = [0, 1, 2, 3]
     choices = [LEFT, RIGHT, UP, DOWN]
+    print('Initialized FruitCollection environment in {:.03f} sec'.format(time.time() - start_time))
 
+    start_time = time.time()
+    print('Initializing DQNAdaptive agent...')
     agent = DQNAdaptive(name="FruitCollecter",
                         choices=choices,
                         network_config=network_config,
@@ -28,7 +33,10 @@ def run_task(evaluation_config, network_config, reinforce_config):
     test_summaries_path = evaluation_config.summaries_path + "/test"
     clear_summary_path(test_summaries_path)
     test_summary_writer = SummaryWriter(test_summaries_path)
+    print('Initialized agent in {:.03f} sec'.format(time.time() - start_time))
 
+    start_time = time.time()
+    print('Training for {} episodes...'.format(evaluation_config.training_episodes))
     # Training Episodes
     for episode in range(evaluation_config.training_episodes):
         state = env.reset(state_representation="linear")
@@ -53,9 +61,12 @@ def run_task(evaluation_config, network_config, reinforce_config):
                                         scalar_value=steps + 1,
                                         global_step=episode + 1)
 
+    print('Completed training in {:.03f} sec'.format(time.time() - start_time))
     agent.disable_learning()
 
     # Test Episodes
+    start_time = time.time()
+    print('Testing for {} episodes...'.format(evaluation_config.training_episodes))
     for episode in range(evaluation_config.test_episodes):
         state = env.reset(state_representation="linear")
         total_reward = 0
@@ -81,5 +92,6 @@ def run_task(evaluation_config, network_config, reinforce_config):
         test_summary_writer.add_scalar(tag="Test/Steps to collect all Fruits",
                                        scalar_value=steps + 1,
                                        global_step=episode + 1)
+    print('Completed testing in {:.03f} sec'.format(time.time() - start_time))
 
     env.close()
