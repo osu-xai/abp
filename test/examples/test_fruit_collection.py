338fc2059fa4e4c1233421091954c58031a28390
Lawrence Neal <nealla@lwneal.com>
2018-10-18 13:41:02 -0700

    Fix fruit collection DQN

--- test/examples/test_fruit_collection.py
+++ test/examples/test_fruit_collection.py
@@ -5,13 +5,13 @@ class FruitCollectionTests(unittest.TestCase):
     def test_fruit_collection_hra(self):
         sys.argv = ['',
                 '--task', 'abp.examples.open_ai.fruit_collection.hra',
-                '--folder', 'test/tasks/fruit_collection_hra_v2']
+                '--folder', 'test/tasks/fruit_collection_hra']
         from abp.trainer.task_runner import main
         main()
 
     def test_fruit_collection_dqn(self):
         sys.argv = ['',
                 '--task', 'abp.examples.open_ai.fruit_collection.dqn',
-                '--folder', 'test/tasks/fruit_collection_dqn_v1']
+                '--folder', 'test/tasks/fruit_collection_dqn']
         from abp.trainer.task_runner import main
         main()
