149733d4e1339f1ad1146379b54d8f98ccb12db2
Lawrence Neal <nealla@lwneal.com>
2018-10-24 19:22:15 -0700

    Allow for ./test.sh -v

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip install -r requirements.txt
-coverage run --source=abp,abp/examples -m unittest discover
+coverage run --source=abp,abp/examples -m unittest discover $*
 coverage report
