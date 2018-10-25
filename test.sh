be846dceb9735edb5a21b0b2cc9d6e41d3555358
Lawrence Neal <nealla@lwneal.com>
2018-10-25 13:10:20 -0700

    Less verbose test output

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pip install -r requirements.txt
-coverage run --source=abp,abp/examples -m unittest discover $*
-coverage report
+pip install -r requirements.txt | grep -v "already satisfied"
+coverage run --source=abp,abp/examples -m unittest discover -v
+coverage report | (head -1 && tail -1)
