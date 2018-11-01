0152bf425580de65d7c181d81b8d1e2483efafb6
Lawrence Neal <nealla@lwneal.com>
2018-11-01 11:55:24 -0700

    Simpler line coverage print
    The head && tail trick only works in some environments

--- test.sh
+++ test.sh
@@ -2,5 +2,7 @@
 visdom &
 pip install -r requirements.txt | grep -v "already satisfied"
 coverage run --source=abp,abp/examples -m unittest discover -v
-coverage report | (head -1 && tail -1)
+echo
+echo "Line Coverage:"
+coverage report | tail -2
 kill %1
