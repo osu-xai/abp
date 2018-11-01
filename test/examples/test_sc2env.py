d68677a2369077449c34c9b769b7df70d1fdc372
Lawrence Neal <nealla@lwneal.com>
2018-11-01 10:08:30 -0700

    Move sc2env example from pysc2 -> sc2env
    The CollectShards example uses Deepmind's pysc2 library.
    The FourTowers examples use our library, sc2env, which uses pysc2
    but also includes extra functionality like automatic mat registration,
    quick resets, and multiple reward signals.

--- test/examples/test_sc2env.py
+++ test/examples/test_sc2env.py
@@ -4,7 +4,7 @@ import unittest
 class SC2EnvTests(unittest.TestCase):
     def test_sc2env_four_towers_hra(self):
         sys.argv = ['',
-                '--task', 'abp.examples.pysc2.four_towers.hra',
+                '--task', 'abp.examples.sc2env.four_towers.hra',
                 '--folder', 'test/tasks/sc2env_four_towers_hra']
         from abp.trainer.task_runner import main
         main()
