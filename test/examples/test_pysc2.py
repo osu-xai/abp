e68ffcfb9525ccbf45944abc6fe67c353b27bd5d
Lawrence Neal <nealla@lwneal.com>
2018-10-31 18:59:39 -0700

    Add failing test for pysc2 collect_shards

---
+++ test/examples/test_pysc2.py
@@ -0,0 +1,10 @@
+import sys
+import unittest
+
+class PySC2Tests(unittest.TestCase):
+    def test_pysc2_shards_dqn(self):
+        sys.argv = ['',
+                '--task', 'abp.examples.pysc2.collect_shards.dqn',
+                '--folder', 'test/tasks/pysc2_collect_shards_dqn']
+        from abp.trainer.task_runner import main
+        main()
