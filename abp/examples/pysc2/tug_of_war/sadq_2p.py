8f010f491c0535b4c83a8e7805ec92ee86b9c96a
khlam <khlam@users.noreply.github.com>
2019-06-24 13:15:37 -0700

    Fix crash, added previous reward to previous state at episode end.

--- abp/examples/pysc2/tug_of_war/sadq_2p.py
+++ abp/examples/pysc2/tug_of_war/sadq_2p.py
@@ -285,10 +285,11 @@ def run_task(evaluation_config, network_config, reinforce_config, map_name = Non
                 previous_reward_1 = current_reward_1
 
             if reinforce_config.collecting_experience:
-                all_experiences.append([
-                    env.denormalization(previous_state_1),
+                experience = [
+                    np.stack((env.denormalization(previous_state_1), previous_reward_1)), 
                     env.denormalization(state_1)
-                ])
+                ]
+                all_experiences.append(experience])
             
             total_rewwards_list.append(total_reward_1)
             test_summary_writer.add_scalar(tag="Test/Episode Reward", scalar_value=total_reward_1,
