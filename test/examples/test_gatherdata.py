b73f76dba162f1ceb83f2dd91041a251d9f11d6a
nick <ngkiddle@gmail.com>
2019-06-27 10:53:19 -0700

    add unit tests back

--- test/examples/test_gatherdata.py
+++ test/examples/test_gatherdata.py
@@ -266,57 +266,54 @@ data4 = np.array(
 )
 class TestGatherData(unittest.TestCase):
 
-    # def test_detect_episode(self):
-    #     ally_wins, enemy_wins, sum_ally_units_win, sum_enemy_units_win = gatherdata.gather_data(data1, 0, 0, 0, 0, 0, len(data1),[])
+    def test_detect_episode(self):
+        ally_wins, enemy_wins, sum_ally_units_win, sum_enemy_units_win = gatherdata.gather_data(data1, 0, 0, 0, 0, 0, len(data1),[])
         
-    #     self.assertEqual(ally_wins, 0)
-    #     self.assertEqual(enemy_wins, 1)
+        self.assertEqual(ally_wins, 0)
+        self.assertEqual(enemy_wins, 1)
 
-    # def test_get_unit_sums_of_winner(self):
-    #     ally_wins, enemy_wins, sum_ally_units_win, sum_enemy_units_win = gatherdata.gather_data(data1, 0, 0, 0, 0, 0, len(data1),[])
+    def test_get_unit_sums_of_winner(self):
+        ally_wins, enemy_wins, sum_ally_units_win, sum_enemy_units_win = gatherdata.gather_data(data1, 0, 0, 0, 0, 0, len(data1),[])
 
-    #     self.assertEqual(sum_enemy_units_win[a_mar]/enemy_wins, 4)
-    #     self.assertEqual(sum_enemy_units_win[a_vik]/enemy_wins, 8)
-    #     self.assertEqual(sum_enemy_units_win[a_col]/enemy_wins, 0)
-    #     self.assertEqual(sum_enemy_units_win[a_pyl]/enemy_wins, 0)
-    #     self.assertEqual(sum_enemy_units_win[a_nex]/enemy_wins, 100)
-    #     self.assertEqual(sum_enemy_units_win[e_mar]/enemy_wins, 4)
-    #     self.assertEqual(sum_enemy_units_win[e_vik]/enemy_wins, 0)
-    #     self.assertEqual(sum_enemy_units_win[e_col]/enemy_wins, 2)
-    #     self.assertEqual(sum_enemy_units_win[e_pyl]/enemy_wins, 3)
-    #     self.assertEqual(sum_enemy_units_win[e_nex]/enemy_wins, 2000)
+        self.assertEqual(sum_enemy_units_win[a_mar]/enemy_wins, 4)
+        self.assertEqual(sum_enemy_units_win[a_vik]/enemy_wins, 8)
+        self.assertEqual(sum_enemy_units_win[a_col]/enemy_wins, 0)
+        self.assertEqual(sum_enemy_units_win[a_pyl]/enemy_wins, 0)
+        self.assertEqual(sum_enemy_units_win[a_nex]/enemy_wins, 100)
+        self.assertEqual(sum_enemy_units_win[e_mar]/enemy_wins, 4)
+        self.assertEqual(sum_enemy_units_win[e_vik]/enemy_wins, 0)
+        self.assertEqual(sum_enemy_units_win[e_col]/enemy_wins, 2)
+        self.assertEqual(sum_enemy_units_win[e_pyl]/enemy_wins, 3)
+        self.assertEqual(sum_enemy_units_win[e_nex]/enemy_wins, 2000)
         
 
-    # def test_get_average_units_of_enemy_winner(self):
-    #     ally_wins, enemy_wins, sum_ally_units_win, sum_enemy_units_win = gatherdata.gather_data(data2, 0, 0, 0, 0, 0, len(data2),[])
+    def test_get_average_units_of_enemy_winner(self):
+        ally_wins, enemy_wins, sum_ally_units_win, sum_enemy_units_win = gatherdata.gather_data(data2, 0, 0, 0, 0, 0, len(data2),[])
         
-    #     self.assertEqual(sum_enemy_units_win[a_mar]/enemy_wins, 6)
-    #     self.assertEqual(sum_enemy_units_win[a_vik]/enemy_wins, 8)
-    #     self.assertEqual(sum_enemy_units_win[a_col]/enemy_wins, 0)
-    #     self.assertEqual(sum_enemy_units_win[a_pyl]/enemy_wins, 0)
-    #     self.assertEqual(sum_enemy_units_win[a_nex]/enemy_wins, 100)
-    #     self.assertEqual(sum_enemy_units_win[e_mar]/enemy_wins, 7.5)
-    #     self.assertEqual(sum_enemy_units_win[e_vik]/enemy_wins, 0)
-    #     self.assertEqual(sum_enemy_units_win[e_col]/enemy_wins, 2)
-    #     self.assertEqual(sum_enemy_units_win[e_pyl]/enemy_wins, 3)
-    #     self.assertEqual(sum_enemy_units_win[e_nex]/enemy_wins, 2000)
+        self.assertEqual(sum_enemy_units_win[a_mar]/enemy_wins, 6)
+        self.assertEqual(sum_enemy_units_win[a_vik]/enemy_wins, 8)
+        self.assertEqual(sum_enemy_units_win[a_col]/enemy_wins, 0)
+        self.assertEqual(sum_enemy_units_win[a_pyl]/enemy_wins, 0)
+        self.assertEqual(sum_enemy_units_win[a_nex]/enemy_wins, 100)
+        self.assertEqual(sum_enemy_units_win[e_mar]/enemy_wins, 7.5)
+        self.assertEqual(sum_enemy_units_win[e_vik]/enemy_wins, 0)
+        self.assertEqual(sum_enemy_units_win[e_col]/enemy_wins, 2)
+        self.assertEqual(sum_enemy_units_win[e_pyl]/enemy_wins, 3)
+        self.assertEqual(sum_enemy_units_win[e_nex]/enemy_wins, 2000)
     
-    # def test_get_average_units_of_enemy_winner(self):
-    #     ally_wins, enemy_wins, sum_ally_units_win, sum_enemy_units_win = gatherdata.gather_data(data3, 0, 0, 0, 0, 0, len(data3),[])
+    def test_get_average_units_of_enemy_winner(self):
+        ally_wins, enemy_wins, sum_ally_units_win, sum_enemy_units_win = gatherdata.gather_data(data3, 0, 0, 0, 0, 0, len(data3),[])
         
-    #     self.assertEqual(sum_ally_units_win[a_mar]/ally_wins, 7.5)
-    #     self.assertEqual(sum_ally_units_win[a_vik]/ally_wins, 4)
-    #     self.assertEqual(sum_ally_units_win[a_col]/ally_wins, 1)
-    #     self.assertEqual(sum_ally_units_win[a_pyl]/ally_wins, 1.5)
-    #     self.assertEqual(sum_ally_units_win[a_nex]/ally_wins, 1950)
-    #     self.assertEqual(sum_ally_units_win[e_mar]/ally_wins, 6)
-    #     self.assertEqual(sum_ally_units_win[e_vik]/ally_wins, 4)
-    #     self.assertEqual(sum_ally_units_win[e_col]/ally_wins, 1)
-    #     self.assertEqual(sum_ally_units_win[e_pyl]/ally_wins, 1.5)
-    #     self.assertEqual(sum_ally_units_win[e_nex]/ally_wins, 55)
-
-    def test_get_strategies_repeat(self):
-        ally_wins, enemy_wins, sum_ally_units_win, sum_enemy_units_win, win_total_timeline, strategies = gatherdata.gather_data(data4, 0, 0, 0, 0, 0, len(data4),[])
+        self.assertEqual(sum_ally_units_win[a_mar]/ally_wins, 7.5)
+        self.assertEqual(sum_ally_units_win[a_vik]/ally_wins, 4)
+        self.assertEqual(sum_ally_units_win[a_col]/ally_wins, 1)
+        self.assertEqual(sum_ally_units_win[a_pyl]/ally_wins, 1.5)
+        self.assertEqual(sum_ally_units_win[a_nex]/ally_wins, 1950)
+        self.assertEqual(sum_ally_units_win[e_mar]/ally_wins, 6)
+        self.assertEqual(sum_ally_units_win[e_vik]/ally_wins, 4)
+        self.assertEqual(sum_ally_units_win[e_col]/ally_wins, 1)
+        self.assertEqual(sum_ally_units_win[e_pyl]/ally_wins, 1.5)
+        self.assertEqual(sum_ally_units_win[e_nex]/ally_wins, 55)
         
 
 
